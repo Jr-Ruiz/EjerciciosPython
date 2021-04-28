5426bef8b738e01b44e8df731650177fe1d9a3a4
José Ramón Ruiz <6961078+Jr-Ruiz@users.noreply.github.com>
2021-04-28 18:41:43 +0200

    Create Ejercicio4.py

---
+++ Ejercicio4.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+import sys
+
+def es_bisiesto(anyo):
+        if anyo%400==0:
+                return True
+        elif anyo%100==0:
+                return False
+        elif anyo%4==0:
+                return True
+        else:
+                return False
+
+print es_bisiesto(int(sys.argv[1]))
